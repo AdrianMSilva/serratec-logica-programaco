195c766bb6e24b6768537a4282ad34f27f83353e
AdrianMSilva <89035745+AdrianMSilva@users.noreply.github.com>
2021-08-20 13:19:37 -0300

    Add files via upload

---
+++ FaseUm/exercicios.js
@@ -0,0 +1,14 @@
+var numero;
+console.log(numero);
+numero = 27;
+console.log(numero);
+numero = 23;
+console.log(numero);
+numero = 0;
+console.log(numero);
+
+//declaração e imediatamente atribuição
+//inicializar a varável
+var documento = "123.456";
+
+var calculadora = 0;
---
+++ FaseZero/olaMundo.js
@@ -0,0 +1,41 @@
+
+//Em JavaScript
+//var <nome da variável, ou identificador>
+
+//Avisando que precisarei guardar dados
+
+var idade;
+var nome;
+var possuiComorbidade;
+var cep;
+var genero;
+
+idade = 38;
+nome = "maicojecson";
+
+string = nome, endereco, bairro;
+var nome, endereco, bairro;
+var nomeUm, nomeDois;
+var nome1, nome2, n_1, n_2;
+integer = numeroCasa;
+
+//recomedno uma em casa linha
+var nomeUm
+var nomeDois
+
+//É possivel fazer mais de uma variável na mesma linha
+var nomeC, nomeD;
+
+Boolean = possuiCarro, estaEmpregado;
+
+//No JS não precisa se preocupar com tipos
+var nome5, numeroCasa, dataNascimento;
+
+//teresinha
+//Inteiros cabe dentro do conjunto dos reais
+idade = 38;
+idade = 30;
+
+console.log("oi pessoal");
+
+console.log(idade);
