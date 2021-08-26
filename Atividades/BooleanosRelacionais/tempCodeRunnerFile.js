e7ee23204caa10f46498c5aebc9e0cb4712029e5
AdrianMSilva <89035745+AdrianMSilva@users.noreply.github.com>
2021-08-26 08:30:24 -0300

    Delete Atividades directory

--- Atividades/BooleanosRelacionais/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-num = 0;
-console.log(num === num); // true
--- Atividades/FaseTrês/calculadorDobro.js
+++
@@ -1,8 +0,0 @@
-
-const ler = require("prompt-sync")();
-
-var numero = ler("Escolha um valor e irei calcular o dobro >:) ");
-
-var dobro = numero * 2;
-
-console.log("O dobro de " + numero + " é " + dobro +". Estou certo né ? >:)")
